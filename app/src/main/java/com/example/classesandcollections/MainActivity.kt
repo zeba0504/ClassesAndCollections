7c40296b80fb9e44777d63737441726c9d85977a
Zeba <saidzodazebida@gmail.com>
2025-11-14 18:25:28 +0300

    Task2

--- app/src/main/java/com/example/classesandcollections/MainActivity.kt
+++ app/src/main/java/com/example/classesandcollections/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.classesandcollections
 
+enum class Daypart {
+    MORNING,
+    AFTERNOON,
+    EVENING,
+}
+
 data class Event(
     val title: String,
     val description: String? = null,
-    val daypart: String,
+    val daypart: Daypart,
     val durationInMinutes: Int,
 )
 
