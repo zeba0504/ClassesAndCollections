207d3787b5b7e528e986372e9b6e0be4937f17cc
Zeba <saidzodazebida@gmail.com>
2025-11-14 18:40:28 +0300

    Task4

--- app/src/main/java/com/example/classesandcollections/MainActivity.kt
+++ app/src/main/java/com/example/classesandcollections/MainActivity.kt
@@ -27,7 +27,17 @@ fun task3(): List<Event> {
     return events
 }
 
-fun main(){
+//Задача 4
+fun task4(events: List<Event>) {
+    val shortEvents = events.filter { it.durationInMinutes < 60 }
+    println("You have ${shortEvents.size} short events.")
+}
 
+
+
+
+fun main(){
+    val events = task3()
+    task4(events)
 
 }
